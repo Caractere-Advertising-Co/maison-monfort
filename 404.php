97c64d57eb7d10e8a4e930d22c35aa506d6c19b3
caractere-advertising <86592307+caractere-advertising@users.noreply.github.com>
2024-06-20 14:16:01 +0200

    get_content

--- 404.php
+++ 404.php
@@ -4,11 +4,17 @@
 $cta = get_field('cta');
 $content = get_field('content');
 
+$text;
+if($content):
+    $text = $content;
+else : 
+    $text = get_the_content();
+endif;
 get_header();?>
 
 <div id="page-404">
     <div class="container">
-        <?php if($content): echo $content; endif;?>
+        <?php echo $text;?>
         <?php if($cta):?>
             <a href="<?php echo $cta['url'];?>" class="cta"><?php echo $cta['title'];?></a>
         <?php endif;?>
