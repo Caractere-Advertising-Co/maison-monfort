e78d9691162a4e5c412133a66406fe5c86d690d0
caractere-advertising <86592307+caractere-advertising@users.noreply.github.com>
2024-09-04 10:42:51 +0200

    activer presentation

--- templates-parts/section-presentation.php
+++ templates-parts/section-presentation.php
@@ -1,4 +1,5 @@
 <?php 
+$actif = get_field('actif-presentation');
 
 $bg = get_field('background-presentation');
 $texte = get_field('texte-presentation');
@@ -7,21 +8,23 @@ $image = get_field('image-presentation');
 
 ?>
 
-<div id="section-presentation" class="from-left">
-    <div class="block-img background">
-        <?php if($bg):?><img src="<?php echo $bg['url'];?>" alt="<?php echo $bg['name'];?>" /><?php endif;?>
-    </div>
-    <div class="container columns">
-        <div class="colg from-left">
-            <?php if($texte): echo $texte; endif;?>
-            <?php if($cta): echo '<a href='.$cta['url'].' class="cta from-bottom">'.$cta['title'].'</a>'; endif;?>
+<?php if($actif):?>
+    <div id="section-presentation" class="from-left">
+        <div class="block-img background">
+            <?php if($bg):?><img src="<?php echo $bg['url'];?>" alt="<?php echo $bg['name'];?>" /><?php endif;?>
         </div>
-        <div class="cold">
-            <div class="block-img from-right">
-                <?php if($image):?>
-                    <img src="<?php echo $image['url'];?>" alt="<?php echo $image['title'];?>" />
-                <?php endif;?>
-            </div>  
+        <div class="container columns">
+            <div class="colg from-left">
+                <?php if($texte): echo $texte; endif;?>
+                <?php if($cta): echo '<a href='.$cta['url'].' class="cta from-bottom">'.$cta['title'].'</a>'; endif;?>
+            </div>
+            <div class="cold">
+                <div class="block-img from-right">
+                    <?php if($image):?>
+                        <img src="<?php echo $image['url'];?>" alt="<?php echo $image['title'];?>" />
+                    <?php endif;?>
+                </div>  
+            </div>
         </div>
     </div>
-</div>
+<?php endif;?>
