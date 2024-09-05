ab490aee249785f82db40c3a5c8c89228f1e8c9c
caractere-advertising <86592307+caractere-advertising@users.noreply.github.com>
2024-09-05 11:30:03 +0200

    inspi

--- templates-parts/section-inspirations.php
+++ templates-parts/section-inspirations.php
@@ -4,6 +4,7 @@ $titre = get_field('titre_inspirations','options');
 
 $galerie;
 $cta;
+$length = [];
 
 if(is_front_page(  )):
     $galerie = get_field('galerie_inspi','options');
@@ -16,38 +17,42 @@ if(is_page(242)):
 else:
     $cta = get_field('cta_inspirations','options');
 endif;
-?>
 
-<div id="section-inspirations">
-    <div class="container from-bottom">
-        <?php if($titre): echo $titre; endif;?>
+$length = count($galerie);
+
+
+if($length > 0):?>
+    <div id="section-inspirations">
+        <div class="container from-bottom">
+            <?php if($titre): echo $titre; endif;?>
+        </div>
+
+        <div class="container-full-width grid">
+            <?php if($galerie):
+                $i = 0;
+                $limit;
+
+                if(is_front_page()):
+                    $limit = 7;
+                else :
+                    $limit = 100;
+                endif;
+                
+                foreach($galerie as $g):
+                    if($i < $limit): ?>
+                        <a data-fslightbox href="<?php echo $g['url'];?>">
+                            <img src="<?php echo $g['url'];?>" alt="<?php echo $g['title'];?>" class="from-bottom"/>
+                        </a>
+                    <?php 
+                    endif; $i++;
+                endforeach;
+            endif;?>
+        </div>
+
+        <div class="container">
+            <?php if($cta):?>
+                <a href="<?php echo $cta['url'];?>" class="cta"><?php echo $cta['title'];?></a>
+            <?php endif;?>
+        </div>
     </div>
-
-    <div class="container-full-width grid">
-        <?php if($galerie):
-            $i = 0;
-            $limit;
-
-            if(is_front_page()):
-                $limit = 7;
-            else :
-                $limit = 100;
-            endif;
-            
-            foreach($galerie as $g):
-                if($i < $limit): ?>
-                    <a data-fslightbox href="<?php echo $g['url'];?>">
-                        <img src="<?php echo $g['url'];?>" alt="<?php echo $g['title'];?>" class="from-bottom"/>
-                    </a>
-                <?php 
-                endif; $i++;
-            endforeach;
-        endif;?>
-    </div>
-
-    <div class="container">
-        <?php if($cta):?>
-            <a href="<?php echo $cta['url'];?>" class="cta"><?php echo $cta['title'];?></a>
-        <?php endif;?>
-    </div>
-</div>
+<?php endif;?>
