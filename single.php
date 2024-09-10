eef6541a0fd9c24b42963de6c89e9c81874acd4f
caractere-advertising <86592307+caractere-advertising@users.noreply.github.com>
2024-09-10 09:35:50 +0200

    infinite loop

--- single.php
+++ single.php
@@ -16,32 +16,83 @@ get_header();?>
         <?php
             $prevPost = get_previous_post(true);
             $nextPost = get_next_post(true);
+
+            if($prevPost):
+                $prev_post_id = $prevPost->ID;
+            else :
+                $abjacent_categories = get_the_category(); 
+                $ids = array();
             
-            if($prevPost) :?>
+                foreach($abjacent_categories as $category) {
+                    $ids[] = $category->term_id;
+                }
+                
+                $last = new WP_Query( array( 
+                    'post_type' => 'post',
+                    'posts_per_page' => 1, 
+                    'orderby'         => 'post_date',
+                    'order'           => 'DESC',
+                    'category__in' => $ids, 
+                )); 
+
+                $last->the_post(); 
+                $prev_post_id = get_the_ID();
+
+                wp_reset_query();
+            endif;
+            
+            if($prev_post_id) :?>
                 <div class="colg">
                     <?php
-                        $title = get_the_title($prevPost->ID);
-                        $textPrev = $prevPost->post_excerpt;
-                        $cta = get_permalink($prevPost->ID);
+                        $title = get_the_title($prev_post_id);
+                        $textPrev = get_the_excerpt($prev_post_id);
+                        $cta = get_permalink($prev_post_id);
                         //$prevthumbnail = get_the_post_thumbnail($prevPost->ID, array(100,100) ); ?>
                     
                     <?php echo '<a href="'.$cta.'" class="ctaTitle"><h3>'.$title .'</h3></a><p>'  .substr($textPrev,0,150) . '...</p>';?>
                     <?php echo '<a href="'.$cta.'" class="cta">Lire plus    </a>';?>
                 </div>
-            <?php endif; ?>
+            <?php endif;
 
-            <?php if($nextPost) : ?>
+            if($nextPost):
+                $next_post_id = $nextPost->ID;
+            else :
+                $abjacent_categories = get_the_category(); 
+                $ids = array();
+            
+                foreach($abjacent_categories as $category) {
+                    $ids[] = $category->term_id;
+                }
+                
+                $last = new WP_Query( array( 
+                    'post_type' => 'post',
+                    'posts_per_page' => 1, 
+                    'orderby'         => 'post_date',
+                    'order'           => 'DESC',
+                    'category__in' => $ids, 
+                )); 
+
+                $last->the_post(); 
+                $next_post_id = get_the_ID();
+
+                wp_reset_query();
+            endif;
+
+            if($next_post_id) : ?>
                 <div class="cold">
                     <?php
-                        $title = get_the_title($nextPost->ID);
-                        $textNext = $nextPost->post_excerpt;
-                        $cta = get_permalink($nextPost->ID );
+                        $title = get_the_title($next_post_id);
+                        $textNext = get_the_excerpt($next_post_id);
+                        $cta = get_permalink($next_post_id);
                         //$prevthumbnail = get_the_post_thumbnail($prevPost->ID, array(100,100) ); ?>
                     
                     <?php echo '<a href="'.$cta.'" class="ctaTitle"><h3>'.$title .'</h3></a><p>'.substr($textNext,0,150) . '... </p>';?>
                     <?php echo '<a href="'.$cta.'" class="cta">Lire plus</a>';?>
                 </div>
             <?php endif; ?>
+
+            
+               
     </div>
 </div>
 
