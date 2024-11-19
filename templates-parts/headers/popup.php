1359e8731ebeca2af2b632cdf7fb96e60d3707d1
caractere-advertising <86592307+caractere-advertising@users.noreply.github.com>
2024-11-19 11:47:14 +0100

    Adaptation css popup

--- templates-parts/headers/popup.php
+++ templates-parts/headers/popup.php
@@ -10,7 +10,7 @@ if($actif):?>
 <?php if(current_user_can( 'administrator')):?>
     <div class="modal_popup" id="modal_popup">
         <div class="centerPopup">
-            <div class="close_popup" id="close_popup"><svg width="20px" height="20px" viewBox="-0.5 0 25 25" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M3 21.32L21 3.32001" stroke="#02a9d9" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path> <path d="M3 3.32001L21 21.32" stroke="#02a9d9" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path> </g></svg></div>
+            <div class="close_popup" id="close_popup"><svg width="20px" height="20px" viewBox="-0.5 0 25 25" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M3 21.32L21 3.32001" stroke="#aa8453" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path> <path d="M3 3.32001L21 21.32" stroke="#aa8453" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path> </g></svg></div>
                 <div class="content_popup">
                     <?php if($img):?><div class="block-img"><img src="<?php echo $img['url'];?>" alt="<?php echo $img['title'];?>"/></div><?php endif;?>
                     <?php if($texte): echo $texte; endif;?>
